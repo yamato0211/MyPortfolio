d4ea9c757b21a3deb91ca63dfd9e59c5656a658c
yamato0211 <kumayama0211@gmail.com>
2023-04-14 16:00:54 +0900

    [change] env key name

--- libs/client.ts
+++ libs/client.ts
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
     serviceDomain: "ottotto",
-    apiKey: process.env.API_KEY!,
+    apiKey: process.env.NEXT_PUBLIC_API_KEY!,
 })
